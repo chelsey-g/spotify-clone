40616a59ce09472b3feaa3e9bea4a30b4b98f4b4
chelsey-g <97612806+chelsey-g@users.noreply.github.com>
2022-09-19 13:44:59 -0400

    added function for my podcasts

--- src/util/spotify.js
+++ src/util/spotify.js
@@ -115,6 +115,9 @@ export function getMyAlbums() {
   return request(`https://api.spotify.com/v1/me/albums`);
 }
 
+export function getMyPodcasts() {
+  return request(`https://api.spotify.com/v1/me/episodes`);
+
 export function getAlbum(id) {
   return request(`https://api.spotify.com/v1/albums/${id}`);
 }
