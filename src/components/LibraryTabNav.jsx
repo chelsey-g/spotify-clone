751b41573ce4e95ddff08daebfef259c5b8f32ee
chelsey-g <97612806+chelsey-g@users.noreply.github.com>
2022-09-29 12:16:13 -0400

    added artists page, fixed navigation links

--- src/components/LibraryTabNav.jsx
+++ src/components/LibraryTabNav.jsx
@@ -4,12 +4,18 @@ import { Link } from "react-router-dom";
 export default function LibraryTabNav(props) {
   return (
     <div className="library-view-tabs">
-      <div className="library-playlist-tab">Playlists</div>
-      <div className="library-podcast-tab">
-        <Link to="collection/podcasts">Podcasts</Link>
+      <div className="library-playlist-tab">
+        <Link to="/collection/playlists">Playlists</Link>
       </div>
-      <div className="library-artist-tab">Artists</div>
-      <div className="library-album-tab">Albums</div>
+      <div className="library-podcast-tab">
+        <Link to="/collection/podcasts">Podcasts</Link>
+      </div>
+      <div className="library-artist-tab">
+        <Link to="/collection/artists">Artists</Link>
+      </div>
+      <div className="library-album-tab">
+        <Link to="/collection/albums">Albums</Link>
+      </div>
     </div>
   );
 }
